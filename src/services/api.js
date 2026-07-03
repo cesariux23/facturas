ce34793038ea47b90c7065710e0b80aab38b826e
César Encarnación <cesariux23@gmail.com>
2026-07-03 10:13:43 -0600

    back

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api', // Replace with your actual API URL
+  baseURL: 'http://api_valida.ivea.local/api', // Replace with your actual API URL
   headers: {
     'Content-Type': 'application/json',
   },
